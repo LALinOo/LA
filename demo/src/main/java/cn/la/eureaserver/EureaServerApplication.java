5e31a434ef51d1c8cccf196b10b47eb98b105762
LALin <lhzero_f@163.com>
2017-12-26 15:06:43 +0800

    eureka Server Create

---
+++ demo/src/main/java/cn/la/eureaserver/EureaServerApplication.java
@@ -0,0 +1,12 @@
+package cn.la.eureaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EureaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EureaServerApplication.class, args);
+	}
+}
